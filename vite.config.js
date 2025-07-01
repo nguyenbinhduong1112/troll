2bd21a6ba297f22e5dcffc2889fe2d38548d1b65
nguyenbinhduong1112 <duongnguyenbinh1112@gmail.com>
2025-07-01 15:11:16 +0700

    fix: clean up index.html, confirm main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detect if running on Vercel or GitHub Pages
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
+  base: isVercel ? '/' : '/troll/',
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/troll/',
 })
