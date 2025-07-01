d15cd118d628feee76f7d3b173e2d37ddf97ee41
nguyenbinhduong1112 <duongnguyenbinh1112@gmail.com>
2025-07-01 14:55:02 +0700

    killurself

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/troll/',
   plugins: [react()],
   base: process.env.VERCEL ? '/' : '/troll/',
 })
